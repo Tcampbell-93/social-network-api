267beda4c24207f9d747cf243757be8b045a0d90
Tcampbell93 <tcampbell_93@hotmail.com>
2024-04-19 09:25:51 -0700

    set up all api routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    createThought,
+    getSingleThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController.js');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction)
+    .delete(deleteReaction);
+
+module.exports = router;
